cc168ae5bc6692247f6fc2e6bbfd83312ca22a2b
Student <example@example.com>
2019-09-18 14:56:58 -0700

    Add images to battle screen

--- src/main.js
+++ src/main.js
@@ -27,6 +27,7 @@ $(document).ready(function() {
     $("#adversary").text("Hippie");
     $("#main-game").removeClass("add-background");
     $("#main-game").addClass("add-skulls");
+    $("#adversary-avatar").html('<img src="https://www.pngkey.com/png/detail/60-608037_stick-figure-running-drawing-download-animation-free-stick.png" alt="a stick figure" height=200px>');
     newPlayer.health = 100;
     let hippie = new enemy.Hippie(100);
     newBattle = new Battle(newPlayer, hippie);
@@ -38,6 +39,7 @@ $(document).ready(function() {
     $("#adversary").text("Hipster");
     $("#main-game").removeClass("add-background");
     $("#main-game").addClass("add-skulls");
+      $("#adversary-avatar").html('<img src="https://t3.ftcdn.net/jpg/00/80/41/34/500_F_80413451_JpH4YrPvmiwINVAny8xpXYa2tFiLbkBl.jpg" height=200px>');
     newPlayer.health = 100;
     let hipster = new enemy.Hipster(100);
     newBattle = new Battle(newPlayer, hipster);
@@ -49,6 +51,7 @@ $(document).ready(function() {
     $("#adversary").text("Professor");
     $("#main-game").removeClass("add-background");
     $("#main-game").addClass("add-skulls");
+      $("#adversary-avatar").html('<img src="https://media.istockphoto.com/vectors/cartoon-illustration-of-old-business-man-or-teacher-or-professor-at-vector-id847759486?k=6&m=847759486&s=612x612&w=0&h=K3-zZwYZZW5lzU46fZk-LHmKO7WwOjSlefCAakdndXQ=" height=200px>');
     newPlayer.health = 100;
     newPlayer.playerHealthBar();
     let professor = new enemy.Professor(100);
@@ -61,6 +64,7 @@ $(document).ready(function() {
     $("#adversary").text("Goth");
     $("#main-game").removeClass("add-background");
     $("#main-game").addClass("add-skulls");
+      $("#adversary-avatar").html('<img src="https://svgsilh.com/svg_v2/154618.svg" height=200px>');
 
     newPlayer.health = 100;
     let singer = new enemy.Goth(100);
@@ -73,6 +77,7 @@ $(document).ready(function() {
     $("#adversary").text("Hacker");
     $("#main-game").removeClass("add-background");
     $("#main-game").addClass("add-skulls");
+      $("#adversary-avatar").html('<img src="https://media.istockphoto.com/vectors/cyber-crime-doodle-vector-id518674300" height=200px>');
     newPlayer.health = 100;
     let hacker = new enemy.Hacker(100);
     newBattle = new Battle(newPlayer, hacker);
